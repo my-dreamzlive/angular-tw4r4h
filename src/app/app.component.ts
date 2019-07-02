4550fd4a1399318360a45e81d6f850e8c12f4302
my-dreamzlive <dreamz9189@yahoo.com>
2019-07-02 13:14:06 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, Renderer2  } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import * as $ from 'jquery';
 import { Apps } from './app.apps';
 import { TitleService } from './title.service';
 import { RouterModule, Routes, Router, NavigationStart, NavigationEnd } from '@angular/router';
@@ -16,13 +15,18 @@ export class AppComponent  {
   constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2, public Apps: Apps, private titleService: TitleService){
     this.App = Apps;
   }
+  addScript(src){
+    let script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    this.renderer.appendChild(this.document.body, script);
+  }
   ngOnInit(): void {
     this.titleService.boot();
     this.renderer.setAttribute(this.document.body,'id','page-top');
-    let script = this.renderer.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
-    this.renderer.appendChild(this.document.body, script);
+    // this.addScript('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js');
+    this.addScript('https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js');
+    this.addScript('https://npmcdn.com/bootstrap@4.0.0-alpha.5/dist/js/bootstrap.min.js');
     this.App.router.events.pipe(
       filter(event => event instanceof NavigationStart)
     ).subscribe(event => {
