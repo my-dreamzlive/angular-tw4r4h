ce6a9e64735a771f50832f38ea7be5bbf1fabfea
my-dreamzlive <dreamz9189@yahoo.com>
2019-07-02 12:14:37 +0530

    5 files modified, 1 added

--- src/app/private/navigation/navigation.component.ts
+++ src/app/private/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Apps } from './../../app.apps';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private app: Apps) { }
 
   ngOnInit() {
   }
