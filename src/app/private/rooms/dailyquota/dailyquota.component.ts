027823aa224cb436c32628e2ea1b64d41e2cf292
my-dreamzlive <dreamz9189@yahoo.com>
2019-07-03 17:05:47 +0530

    4 files modified

--- src/app/private/rooms/dailyquota/dailyquota.component.ts
+++ src/app/private/rooms/dailyquota/dailyquota.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Apps } from './../../../app.apps';
 @Component({
   selector: 'app-dailyquota',
   templateUrl: './dailyquota.component.html',
   styleUrls: ['./dailyquota.component.css']
 })
 export class DailyquotaComponent implements OnInit {
-
-  constructor() { }
+  dailyquota;
+  constructor(public app: Apps) { }
 
   ngOnInit() {
+    //this.app.options = {headers: this.app.headers, responseType:'text'};
+    let httpResp = new Promise((resolve)=>{
+      this.app.getResponse("master::check::dailyquota").subscribe((res)=>{
+        resolve(res);
+      });
+    });
+    httpResp.then(res=>{
+      console.log(res);
+    });
   }
 
 }
--- src/app/private/topbar/topbar.component.ts
+++ src/app/private/topbar/topbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Apps } from './../../app.apps';
-import * as $ from 'jquery';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
