0fe650ef771c88b4dae446f34da233bdbfd38a52
my-dreamzlive <dreamz9189@yahoo.com>
2019-07-06 00:17:21 +0530

    1 file modified

--- src/app/private/rooms/roomstype/roomstype.component.ts
+++ src/app/private/rooms/roomstype/roomstype.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Apps } from './../../../app.apps';
 @Component({
   selector: 'app-roomstype',
   templateUrl: './roomstype.component.html',
   styleUrls: ['./roomstype.component.css']
 })
 export class RoomstypeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  _new: any;
+  rooms = [];
+  constructor(public app: Apps) {
+    
   }
-
+  roomlist(){
+    let httpResp = new Promise((resolve)=>{
+      this.app.getResponse("master::get::roomlist").subscribe((res)=>{
+        resolve(res);
+      });
+    });
+    httpResp.then((res: any)=>{
+      this.rooms = res;
+    });
+  }
+  ngOnInit() {
+    this._new = [];
+    this.roomlist();
+  }
+  newRoom(){
+    console.log(this._new);
+  }
 }
