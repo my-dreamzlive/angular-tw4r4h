38ece1dbd50077edcffeb117873b0a0680d1e8ef
my-dreamzlive <dreamz9189@yahoo.com>
2019-07-06 19:02:52 +0530

    2 files modified

--- src/app/private/rooms/roomstype/roomstype.component.ts
+++ src/app/private/rooms/roomstype/roomstype.component.ts
@@ -31,9 +31,27 @@ export class RoomstypeComponent implements OnInit {
     
   }
   changeRoom(val = null, i = null){
-    this.changed = true;
-    this.rooms[i]['code'] = this.app.filterCode(this.rooms[i]['code']);
-    this.rooms[i]['available'] = this.app.filterDigits(this.rooms[i]['available'].toString());
+    this.rooms[i]['invalid'] = [];
+    Object.entries(this.rooms[i]).forEach((param) => {
+      if(typeof(this.rooms[i][param[1]])==''){
+        this.rooms[i][param[0]] = null;
+        
+      }
+      if(this.rooms[i][param[0]] == null){
+        this.rooms[i]['invalid'].push(param[0]);
+      }
+    });
+    let nullcount = Object.values(this.rooms[i]).indexOf(null);
+    console.log(nullcount);
+    let okeys:any = ['id','code','type','detail','available','rent','status'];
+    
+      this.changed = true;
+      this.rooms[i]['code'] = this.app.filterCode(this.rooms[i]['code'].toString());
+      
+      this.rooms[i]['rent'] = this.app.filterCurrency(this.rooms[i]['rent'].toString());
+      this.rooms[i]['available'] = this.app.filterDigits(this.rooms[i]['available'].toString());
+    
+    
     
     console.log(this.rooms[i]);
   }
